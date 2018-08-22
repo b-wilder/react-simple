de782245ade5664ac1f8bd232c5f7df6e76c3a6b
Brock Elgart <bwelgart@gmail.com>
2018-08-22 09:46:35 -0400

    attempted build

---
+++ src/js/components/InputForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class InputForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.props.setMessageCallback(document.getElementById('submitVal').value);
+  }
+
+  render() {
+    const messageVal = this.props.message;
+    return(
+      <form>
+        <input id="submitVal" type="text" value={messageVal}/>
+        <button name="Submit" type="button" onClick={ this.handleClick }>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
